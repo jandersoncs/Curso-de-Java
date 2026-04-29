e40d1127276f45ff95863dadf57b08bfce859840
jcruz <jandersoncruz15@gmail.com>
2026-04-29 11:14:25 -0300

    Inclusão da aula, metodos static.

---
+++ src/main/java/metodosestaticos/Main.java
@@ -0,0 +1,9 @@
+package main.java.metodosestaticos;
+import static main.java.metodosestaticos.TesteStatic.exibirValor;
+
+public class Main {
+    public static void main(String[] args){
+        exibirValor(10);
+        exibirValor("Joao");
+    }
+}
---
+++ src/main/java/metodosestaticos/TesteStatic.java
@@ -0,0 +1,14 @@
+package main.java.metodosestaticos;
+
+public class TesteStatic {
+    public static void exibirValor(int valor){
+        System.out.println("O numero inteiro é: "+valor);
+    }
+    public static void exibirValor(String valor){
+        System.out.println("A String é: "+valor);
+    }
+
+
+
+
+}
